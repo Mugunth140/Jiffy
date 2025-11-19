3557780d526d3bbe3bda9b79c1ec3ff0825d5463
Mugunth140 <mugunth140@gmail.com>
2025-11-19 23:29:00 +0530

    Init: Project Initialize

---
+++ app/components/onBoarding.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
+
+const onBoarding = () => {
+  return (
+    <SafeAreaView>
+      <Text>Onboarding Screen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default onBoarding
---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Text } from "react-native";
+
+export default function Index() {
+  return (
+     <SafeAreaView className="flex-1 items-center justify-center bg-white">
+      <Text className="text-5xl font-medium text-blue-500">
+        Welcome to Jiffy
+      </Text>
+    </SafeAreaView>
+  );
+}
